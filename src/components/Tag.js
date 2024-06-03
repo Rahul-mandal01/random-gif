c5ad8b29aa8f9b27db8055b4794c3e8e5911b31e
Rahul Kumar Mandal <rahulra16809999@gmail.com>
2024-06-03 21:07:51 +0530

    Updated JS and CSS

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
+
 
 const Tag = () => {
+
+  const [tag, setTag] = useState('Welcome');
+  const {gif, loading, fetchData} = useGif(tag);
+
+
   return (
-    <div></div>
+    <div  className='w-11/12 bg-blue-500 rounded-lg border border-black 
+    flex flex-col items-center gap-y-5 mt-[15px] py-4 ' >
+      <h1 className='text-2xl underline uppercase font-bold mt-[15px] ' > Random {tag} GIF</h1>
+
+      {
+        loading? (<Spinner/>):(<img src={gif} width="450" alt=''  />)
+      }
+
+      <input 
+        className=' w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center '
+        onChange = {(event) => setTag(event.target.value)}
+        value ={tag}
+
+
+      />
+
+      <button onClick={() => fetchData(tag) } 
+        className='w-10/12 bg-red-400 text-lg rounded-md py-2 mb-[20px] '>
+          Generate
+        </button>
+    </div>
   )
 }
-
 
 export default Tag;
